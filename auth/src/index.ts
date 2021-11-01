3b5cd3a322b9dc80d91a3e3054bda68105f68258
rahul kathait <edendattox@gmail.com>
2021-11-01 11:49:19 +0530

    added error-handler middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,21 +1,36 @@
 import express from "express";
 import { json } from "body-parser";
+
+/**
+ * ErrorHandler
+ */
+
+import { errorHandler } from "./middlewares/error-handler";
+
+/**
+ * Routes
+ */
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 
 const app = express();
-app.use(json());
 
 /**
  * Routers
- * */
+ */
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+/**
+ *  Middlewares
+ */
+app.use(json());
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
