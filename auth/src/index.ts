c6a0e38b73df6303b44d0aade77337be365170ac
rahul kathait <edendattox@gmail.com>
2021-11-01 10:18:32 +0530

    added ingress-srv config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,14 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+/**
+ *  routes
+ */
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("hi there");
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
 });
