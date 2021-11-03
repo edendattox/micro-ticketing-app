d98e698abca8a86cfbe165eb74950093cc3a70ed
rahul kathait <edendattox@gmail.com>
2021-11-03 10:19:31 +0530

    connected to the database

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,7 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
-
-/**
- * ErrorHandler
- */
-
-import { errorHandler } from "./middlewares/error-handler";
+import mongoose from "mongoose";
 
 /**
  * Routes
@@ -14,6 +10,13 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+
+/**
+ * ErrorHandler
+ */
+
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -28,10 +31,39 @@ app.use(signoutRouter);
 app.use(signupRouter);
 
 /**
+ *  Create error for the route that does'nt exits.
+ *  app.all gonna watch for any type of method for req
+ */
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+/**
  *  Middlewares
  */
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!");
-});
+/**
+ *  Connecting to the mongoose database
+ */
+
+const start = async () => {
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  };
+
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", options);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!");
+  });
+};
+
+start();
