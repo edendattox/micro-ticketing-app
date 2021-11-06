c954f48d547568accaef324fc0f3935df482bd66
rahul kathait <edendattox@gmail.com>
2021-11-06 14:01:41 +0530

    Made some changes

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -44,7 +44,7 @@ it("responds with cookie when given valid credentials", async () => {
       email: "test@example.com",
       password: "password",
     })
-    .expect(201);
+    .expect(200);
 
   expect(response.get("Set-Cookie")).toBeDefined();
 });
