7a15d6d862fd3e9403b80dec10e84487e7d30362
rahul kathait <edendattox@gmail.com>
2021-11-06 11:55:16 +0530

    Created test setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,32 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = "askjfaf";
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  };
+
+  await mongoose.connect(mongoUri, options);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
