bf6e8fb7b7fb9b60ce41bc759331ed40f2b9ce1a
rahul kathait <edendattox@gmail.com>
2021-11-25 19:53:12 +0530

    Modiefied code

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 console.clear();
 
@@ -15,24 +16,47 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('')
-
-  const subscription = stan.subscribe("ticket:created", options);
-
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(`Received event ${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
+
+// import nats, { Message, Stan } from "node-nats-streaming";
+// import { randomBytes } from "crypto";
+
+// console.clear();
+
+// const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
+//   url: "http://localhost:4222",
+// });
+
+// stan.on("connect", () => {
+//   console.log("Listener connected to NATS");
+
+//   stan.on("close", () => {
+//     console.log("NATS connection closed");
+//     process.exit();
+//   });
+
+//   const options = stan
+//     .subscriptionOptions()
+//     .setManualAckMode(true)
+//     .setDeliverAllAvailable()
+//     .setDurableName("");
+
+//   const subscription = stan.subscribe("ticket:created", options);
+
+//   subscription.on("message", (msg: Message) => {
+//     const data = msg.getData();
+
+//     if (typeof data === "string") {
+//       console.log(`Received event ${msg.getSequence()}, with data: ${data}`);
+//     }
+
+//     msg.ack();
+//   });
+// });
+
+// process.on("SIGINT", () => stan.close());
+// process.on("SIGTERM", () => stan.close());
