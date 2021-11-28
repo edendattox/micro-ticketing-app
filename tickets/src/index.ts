5da65c5623d56baf2d58307c86e0d90f8a030771
rahul kathait <edendattox@gmail.com>
2021-11-28 09:29:35 +0530

    Updated code

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -17,6 +18,16 @@ const start = async () => {
   };
 
   try {
+    await natsWrapper.connect("ticketing", "haf", "http://nats-srv:4222");
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close()); // SIGINT = Interrupt
+    process.on("SIGTERM", () => natsWrapper.client.close()); // SIGTERM = Terminate
+
     await mongoose.connect(process.env.MONGO_URI, options);
     console.log("Connected to MongoDB");
   } catch (err) {
