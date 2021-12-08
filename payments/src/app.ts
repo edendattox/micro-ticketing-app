3cf28fede4f45f995ce9d73bb759d51ca85b2004
rahul kathait <edendattox@gmail.com>
2021-12-08 10:50:15 +0530

    Created payments folder

---
+++ payments/src/app.ts
@@ -0,0 +1,57 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+/**
+ * ErrorHandlers
+ */
+
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@eden-d-tickets/common";
+
+/**
+ * Routes
+ */
+// import { createTicketRouter } from "./routes/new";
+// import { showTicketRouter } from "./routes/show";
+// import { indexTicketRouter } from "./routes/index";
+// import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(currentUser);
+
+/**
+ * Routers
+ */
+
+// app.use(createTicketRouter);
+// app.use(showTicketRouter);
+// app.use(indexTicketRouter);
+// app.use(updateTicketRouter);
+
+/**
+ *  Create error for the route that does'nt exits.
+ *  app.all gonna watch for any type of method for req
+ */
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+/**
+ *  Middlewares
+ */
+app.use(errorHandler);
+
+export { app };
